ed982fc3f19bc4c3e1834471480ada6c0aac4495
nrao <nrao@nd.edu>
2018-12-03 23:18:11 -0500

    Added title and subtitle, working with @angular/material to add a theme and room tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SPECTREui';
+  title = 'Hesburgh Library Room Occupancy';
+  subtitle = 'SPECTRE CPEG Senior Design Fall 2018'
 }
