51cde39d7a670b0e01c76c4c857642d891abc971
nrao <nrao@nd.edu>
2018-12-06 16:23:25 -0500

    updates to get nygraphs working in the html and script folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiCallService} from './api-call.service';
 import { Title } from '@angular/platform-browser';
-import DateTimeFormat = Intl.DateTimeFormat;
 
 @Component({
   selector: 'app-root',
@@ -18,9 +17,20 @@ export class AppComponent implements OnInit {
   timestamp = 0;
   date = 0;
   realDate;
-  dateData: Date[];
-  peopleData: number[];
-  i = 0;
+  // public dateData: [][];
+  // peopleData: number[];
+  // i = 0;
+  data = [
+    [new Date('2:00:00PM'), 17],
+    [new Date('3:00:00PM'), 19],
+    [new Date('4:00:00PM'), 22],
+    [new Date('5:00:00PM'), 15],
+    [new Date('6:00:00PM'), 9],
+    [new Date('7:00:00PM'), 10],
+    [new Date('8:00:00PM'), 16],
+    [new Date('9:00:00PM'), 20],
+  ];
+
   options = {width: 'auto',
               labels: ['x', 'y'],
               xlabel: 'time',
@@ -31,7 +41,6 @@ export class AppComponent implements OnInit {
   constructor(private titleService: Title,  private service: ApiCallService) {}
 
   ngOnInit() {
-
     this.service.get().subscribe(x => {
       this.count = x['Count'];
     });
@@ -39,10 +48,18 @@ export class AppComponent implements OnInit {
     this.service.get().subscribe(x => {
       this.numPeople = x['Items'][0]['numpeople']['N'];
       this.capacityPercentage = this.numPeople * 100 / this.capacity;
+      console.log(this.numPeople);
     });
-    console.log(this.numPeople);
 
-    for (this.i; this.i < this.count; this.i + 1) {
+    this.service.get().subscribe(y => {
+      this.timestamp = y['Items'][0]['timestamp']['N'];
+      this.date = this.timestamp - 18000;
+      this.realDate = new Date(this.date).toLocaleTimeString('en-US');
+      console.log(this.realDate);
+    });
+
+    /*
+    for ( this.i; this.i < this.count; this.i + 1) {
       this.service.get().subscribe(x => {
         this.numPeople = x['Items'][this.i]['numpeople']['N'];
         this.peopleData[this.i] = this.numPeople;
@@ -54,7 +71,6 @@ export class AppComponent implements OnInit {
         this.dateData[this.i] = this.realDate;
       });
     }
-    console.log(this.peopleData);
-    console.log(this.dateData);
+    */
   }
 }
